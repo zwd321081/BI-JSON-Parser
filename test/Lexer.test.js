00976fb3046cfca6b093ed7fc751c76eb79e8abf
zwd <zwd321081@gmail.com>
2019-07-29 16:56:02 +0800

    feat: add jest test

---
+++ test/Lexer.test.js
@@ -0,0 +1,14 @@
+import Lexer from "../src/Lexer"
+test('get simple Token',()=>{
+    const input = `{
+        "page": "dailysongrecommend|userfm", // 日推|私人FM
+        "resource": "song", // 歌曲
+        "resourceid": true, // 歌曲id，如123
+        "target": 111, // 不感兴趣
+        "targetid": "button", // 点击按钮
+        "reason": "", // 不感兴趣理由，如推荐重复
+        "reason_type": "fixed" // 不感兴趣理由类型，固定
+      }`
+    let lexer = new Lexer(input);
+    expect(lexer.getNextToken()).toBe('{');
+})
